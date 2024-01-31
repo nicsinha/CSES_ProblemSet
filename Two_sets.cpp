9cb5d08f895a1846c48b40912cf8979b58c74b21
nicsinha <sinhanikhil097@gmail.com>
2024-01-31 21:53:56 +0530

    Implemeted subsequence

--- Two_sets.cpp
+++ Two_sets.cpp
@@ -10,6 +10,30 @@ void print_vector(vector<int> v1,vector<int> v2) {
     for(auto i:v2) {cout<<i<<" ";}  
 }
 
+int flag = 1;
+vector<int> ans;
+void findSubsequencewithSum_k(vector<int> v, vector<int> &v1, int i, int sum, int k, int n) {
+
+    
+    if(i == n) {
+        cout<<"Sum = "<<sum<<endl;
+        if(k == sum) {
+            if(flag) {ans = v1;flag = 0;}
+            return;
+        }
+        return;
+    }
+
+    v1.push_back(v[i]);
+    
+    findSubsequencewithSum_k(v,v1,i+1,sum+=v[i],k,n);
+    
+    v1.pop_back();
+
+    findSubsequencewithSum_k(v,v1,i+1,sum-=v[i],k,n);
+
+}
+
 int main() {
 
     int n;
@@ -43,23 +67,31 @@ int main() {
     }
     else {
 
-        int sum = (n*(n+1))/4;
-        vector<bool> v3(n+1,false);
+        int k = (n*(n+1))/4;
+        // vector<bool> v3(n+1,false);
+        cout<<" k = "<<k;
+        // for(int i=n;i>0;i--) {
+        //     if(i <= sum) {
+        //         v1.push_back(i);
+        //         v3[i] = true;
+        //         sum -= i;
+        //     }
+        // }
 
-        for(int i=n;i>0;i--) {
-            if(i <= sum) {
-                v1.push_back(i);
-                v3[i] = true;
-                sum -= i;
-            }
-        }
+        // for(int i=1;i<=n;i++) {
+        //     if(!v3[i]) {
+        //         v2.push_back(i);
+        //     }
+        // }
+        // print_vector(v1,v2);
 
-        for(int i=1;i<=n;i++) {
-            if(!v3[i]) {
-                v2.push_back(i);
-            }
-        }
-        print_vector(v1,v2);
+        vector<int> v(n+1,0),v1;
+        for(int i=1;i<=n;i++) {v[i-1] = i;}
+        
+        findSubsequencewithSum_k(v,v1,0,0,k,n);
+
+        for(auto i:ans) {cout<<i<<" ";}
+
     }
 
 
